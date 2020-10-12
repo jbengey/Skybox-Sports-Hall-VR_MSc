ea9c42a133179649ec6e7095e2f733c44a950d57
Joe <919946@swansea.ac.uk>
2020-10-12 15:11:30 +0100

    Pushing changes to the console print

--- Assets/helloconsole.cs
+++ Assets/helloconsole.cs
@@ -7,7 +7,8 @@ public class helloconsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World - Jack B");
+        // write to console
+        Debug.Log("919946");
     }
 
     // Update is called once per frame
